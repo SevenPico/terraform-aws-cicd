88007d9ba1fe93c1b028be903900617c1611048f
Dave Raring <d@7pi.co>
2023-04-13 16:35:07 -0500

    fixing count issues

--- pipelines.tf
+++ pipelines.tf
@@ -28,8 +28,8 @@ locals {
     { for k, v in var.s3_targets : "${module.context.id}/s3/${k}" => "${v.source_s3_bucket_id}/${v.source_s3_object_key}" },
   )
 
-  ecs_target_version_ssm_parameter_names_map = { for k, v in var.ecs_targets : k => aws_ssm_parameter.target_source["${module.context.id}/ecs/${k}"].name }
-  s3_target_version_ssm_parameter_names_map= { for k, v in var.s3_targets : k => aws_ssm_parameter.target_source["${module.context.id}/s3/${k}"].name }
+  ecs_target_version_ssm_parameter_names_map = module.context.enabled ? { for k, v in var.ecs_targets : k => aws_ssm_parameter.target_source["${module.context.id}/ecs/${k}"].name } : {}
+  s3_target_version_ssm_parameter_names_map= module.context.enabled ? { for k, v in var.s3_targets : k => aws_ssm_parameter.target_source["${module.context.id}/s3/${k}"].name } : {}
 }
 
 
