9e8e92bc58add63f2464e78dba1a85a2ed671611
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-07-11 17:02:03 +0530

    testing code build module

--- modules/s3-pipeline/pre-deploy.tf
+++ modules/s3-pipeline/pre-deploy.tf
@@ -52,8 +52,9 @@
 # Codebuild Project
 # -----------------------------------------------------------------------------
 module "pre_deploy_codebuild" {
-  source     = "registry.terraform.io/SevenPico/codebuild/aws"
-  version    = "2.0.0"
+#  source     = "registry.terraform.io/SevenPico/codebuild/aws"
+#  version    = "2.0.0"
+  source      = "git::https://github.com/SevenPico/terraform-aws-codebuild.git?ref=hotfix/2.0.1"
   context    = module.context.self
   enabled    = module.context.enabled && var.pre_deploy_enabled
   attributes = ["pre-deploy"]
