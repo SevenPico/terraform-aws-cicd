1463c881f95168ac8342e581e6f70a78373c4059
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-07-13 00:50:06 +0530

    error fixed

--- pipelines.tf
+++ pipelines.tf
@@ -26,7 +26,7 @@ locals {
   targets = merge(
     { for k, v in var.ecs_targets : "${module.context.id}/ecs/${k}" => v.image_uri },
     { for k, v in var.s3_targets : "${module.context.id}/s3/${k}" => v.ssm_artifact_uri_value }, #"${v.source_s3_bucket_id}/${v.source_s3_object_key}" },
-    { for k, v in var.cloudformation_targets : "${module.context.id}/cloudformation/${k}" => v.source_s3_bucket_id/v.source_s3_object_key },
+    { for k, v in var.cloudformation_targets : "${module.context.id}/cloudformation/${k}" => "${v.source_s3_bucket_id}/${v.source_s3_object_key}"},
   )
 
   ecs_target_version_ssm_parameter_names_map = module.context.enabled ? { for k, v in var.ecs_targets : k => aws_ssm_parameter.target_source["${module.context.id}/ecs/${k}"].name } : {}
