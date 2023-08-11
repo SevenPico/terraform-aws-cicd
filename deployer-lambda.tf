793c6b1b3fecaada0d64a157a68401bbc10178f8
adarshiwralesigmasolve07 <107871303+adarshiwralesigmasolve07@users.noreply.github.com>
2023-08-11 21:34:35 +0530

    Hotfix/1.1.4 (#14)
    * testing star
    
    * commit
    
    * pre deploy steps addedto cf
    
    * pre deploy object
    
    * reverted to *

--- deployer-lambda.tf
+++ deployer-lambda.tf
@@ -227,9 +227,20 @@ module "deployer_lambda_policy" {
       ]
     }
     "S3GetArtifact" = {
-      effect  = "Allow"
-      actions = ["s3:Get*"]
-      resources = ["*"]
+      effect    = "Allow"
+      actions   = ["s3:Get*"]
+      resources = ["*"] #Fixme Allow only local.targets buckets.
+      #      resources = concat(
+      #        [
+      #          for target in values(var.s3_targets) : "arn:aws:s3:::${target.source_s3_bucket_id}/*"
+      #        ],
+      #        [
+      #          for target in values(var.cloudformation_targets) : "arn:aws:s3:::${target.source_s3_bucket_id}/*"
+      #        ],
+      #        [
+      #          for target in values(var.ec2_targets) : "arn:aws:s3:::${target.source_s3_bucket_id}/*"
+      #        ]
+      #      )
     }
   }
 }
--- modules/cf-pipeline/pipeline.tf
+++ modules/cf-pipeline/pipeline.tf
@@ -5,51 +5,77 @@ module "pipeline" {
   artifact_store_s3_bucket_id    = var.artifact_store_s3_bucket_id
   artifact_store_kms_key_arn     = var.artifact_store_kms_key_arn
   cloudwatch_log_expiration_days = var.cloudwatch_log_expiration_days
-  iam_policy_statements          = {}
+  iam_policy_statements = var.pre_deploy_enabled ? {
+    codebuild = {
+      effect    = "Allow"
+      actions   = ["codebuild:*"]
+      resources = [module.pre_deploy_codebuild.project_arn]
+    }
+  } : {}
 
   stages = [
-    {
-      name = "source"
-      actions = {
-        cf-source = {
-          category = "Source"
-          owner    = "AWS"
-          provider = "S3"
-          version  = "1"
+    for stage in [
+      {
+        name = "source"
+        actions = {
+          s3-source = {
+            category = "Source"
+            owner    = "AWS"
+            provider = "S3"
+            version  = "1"
 
-          input_artifacts  = []
-          output_artifacts = ["source"]
+            input_artifacts  = []
+            output_artifacts = ["source"]
 
-          configuration = {
-            S3Bucket    = var.source_s3_bucket_id
-            S3ObjectKey = var.source_s3_object_key
+            configuration = {
+              S3Bucket    = var.source_s3_bucket_id
+              S3ObjectKey = var.source_s3_object_key
+            }
           }
         }
-      }
-    },
-    {
-      name = "deploy"
-      actions = {
-        cf-deploy = {
-          category = "Deploy"
-          owner    = "AWS"
-          provider = "CloudFormation"
-          version  = "1"
+      },
 
-          input_artifacts  = ["source"]
-          output_artifacts = []
+      var.pre_deploy_enabled ? {
+        name = "pre-deploy"
+        actions = {
+          codebuild = {
+            category = "Build"
+            owner    = "AWS"
+            provider = "CodeBuild"
+            version  = "1"
 
-          configuration = {
-            ActionMode         = var.cloudformation_action_mode
-            Capabilities       = var.cloudformation_capabilities
-            ChangeSetName      = "Cloudformation-Stack-Changes"
-            ParameterOverrides = var.cloudformation_parameter_overrides
-            RoleArn            = var.cloudformation_role_arn
-            StackName          = var.cloudformation_stack_name
-            TemplatePath       = "source::${var.cloudformation_template_name}"
+            input_artifacts  = ["source"]
+            output_artifacts = ["pre-deploy"]
+
+            configuration = {
+              ProjectName = module.pre_deploy_codebuild.project_name
+            }
           }
         }
-      }
-    },
-  ]
+      } : null,
+      {
+        name = "deploy"
+        actions = {
+          cf-deploy = {
+            category = "Deploy"
+            owner    = "AWS"
+            provider = "CloudFormation"
+            version  = "1"
+
+            input_artifacts  = ["source"]
+            output_artifacts = []
+
+            configuration = {
+              ActionMode         = var.cloudformation_action_mode
+              Capabilities       = var.cloudformation_capabilities
+              ChangeSetName      = "Cloudformation-Stack-Changes"
+              ParameterOverrides = var.cloudformation_parameter_overrides
+              RoleArn            = var.cloudformation_role_arn
+              StackName          = var.cloudformation_stack_name
+              TemplatePath       = "source::${var.cloudformation_template_name}"
+            }
+          }
+        }
+      },
+  ] : stage if stage != null]
 }
